76ed156e7a8ea1ab25683c08d610eb7816b0852b
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-09-08 10:49:11 -0300

    Definindo a classe TabuadaDeSoma

---
+++ Java/TabuadaDeSoma.java
@@ -0,0 +1,42 @@
+/**
+ * Classe que imprime a tabuada de soma de um número entre 1 e 10.
+ *
+ * @author Jessé Lopes
+ */
+
+import java.util.Scanner;
+
+public class TabuadaDeSoma {
+
+  public static void main(String[] args) {
+
+    try(Scanner scanner = new Scanner(System.in)) {
+
+      int numero;
+
+      do {
+        System.out.print("\n" + "Digite um número entre 1 e 10: ");
+        numero = scanner.nextInt();
+
+        if (numero <= 10) {
+            System.out.println("\n" + "A tabuada do número " + numero + " é:");
+        } else {
+          while (numero > 10) {
+            System.out.println("\n" + "Observação:");
+            System.out.println("\n" + "Só é permitido um número entre 1 e 10. Tente novamente!");
+            System.out.print("\n" + "Digite um número entre 1 e 10: ");
+            numero = scanner.nextInt();
+            numero++;
+          }
+
+          System.out.print("\n" + "A tabuada do número " + numero + " é:");
+        }
+
+        for (int contador = 1; contador <= 10; contador++) {
+          System.out.println("\n" + numero + " + " + contador + " = " + (numero + contador));
+        }
+
+      } while (numero <= 10);
+    }
+  }
+}
