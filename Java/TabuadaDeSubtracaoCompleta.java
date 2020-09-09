02758792941d7a2c6aac0da10904e0d57b527663
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-09-09 13:34:43 -0300

    Definindo a classe TabuadaDeSubtracaoCompleta.java

---
+++ Java/TabuadaDeSubtracaoCompleta.java
@@ -0,0 +1,18 @@
+/**
+ * Classe que imprime a tabuada de subtracao completa dos números de 0 e 10.
+ *
+ * @author Jessé Lopes
+ */
+
+public class TabuadaDeSubtracaoCompleta {
+
+    public static void main(String[] args) {
+  
+      for (int unidade = 0; unidade <= 10; unidade++) {
+        System.out.println("\n" + "A tabuada do número " + unidade + " é:" + "\n");
+        for (int dezena = 0; dezena <= 10; dezena++) {
+          System.out.println(unidade + " - " + dezena + " = " + (unidade - dezena));
+        }
+      }
+    }
+  }
